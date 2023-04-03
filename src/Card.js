8daf1cc153822540bd2c4a7f6f9bff8638d64ba5
bhaskar <bhaskaragodasi@gmail.com>
2023-04-03 16:17:33 +0530

    updating

--- src/Card.js
+++ src/Card.js
@@ -10,10 +10,12 @@ export default function Card({ data }) {
 
         (data.length > 0) ? data.map((val) => {
           return (<div class="card" style={{ width: "18rem" }}>
-            {/* <img src={val.avatar} class="card-img-top" alt="image not found " /> */}
+            <img src={val.avatar} class="card-img-top" alt="image not found " />
             <div class="card-body">
               <h5 class="card-title">{val.first_name}{val.last_name}</h5>
               <p class="card-text">{val.email}</p>
+<h1> we are ready</h1>
+
             </div>
           </div>
           )
