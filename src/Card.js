927cf2472a3d91115c5244464f4ad56cb1f1e3cc
bhaskar <bhaskaragodasi@gmail.com>
2023-04-03 12:35:29 +0530

    login2

--- src/Card.js
+++ src/Card.js
@@ -10,7 +10,7 @@ export default function Card({ data }) {
 
         (data.length > 0) ? data.map((val) => {
           return (<div class="card" style={{ width: "18rem" }}>
-            <img src={val.avatar} class="card-img-top" alt="image not found " />
+            {/* <img src={val.avatar} class="card-img-top" alt="image not found " /> */}
             <div class="card-body">
               <h5 class="card-title">{val.first_name}{val.last_name}</h5>
               <p class="card-text">{val.email}</p>
